cb51c6ee969c713bf7c1536743310e139b664060
mahara004 <mahara004@gmail.com>
2021-03-17 19:36:54 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive=1
+
+next.addEventListener('click',() =>{
+    currentActive++;
+
+    if(currentActive >circles.length){
+        currentActive=circles.length
+    
+        }
+        console.log(circles.length);
+        console.log(currentActive);
+
+        update();
+})
+
+prev.addEventListener('click',() =>{
+    currentActive--;
+
+    if(currentActive <1){
+        currentActive=1;
+        }
+        console.log(currentActive);
+
+        update();
+})
+
+function update() {
+    circles.forEach( (circle,idx) =>{
+        console.log("id:"+idx);
+        if(idx< currentActive){
+            circle.classList.add('active');
+        }
+        else{
+            circle.classList.remove('active');
+        }
+    })
+
+    const active = document.querySelectorAll('.active')
+    progress.style.width = (active.length-1) / (circles.length-1) *100 +'%'
+
+    if(currentActive === 1){
+        prev.disabled=true;
+    }
+    else if(currentActive === circles.length){
+        next.disabled=true;
+    }
+    else{
+        prev.disabled=false;
+        next.disabled=false;
+    }
+}
